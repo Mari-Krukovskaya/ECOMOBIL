4a2e6a6e06b3468e93f9bce12479eac1fbb9abee
Maria Kruk <mari.krukovskaya.97@mail.ru>
2024-02-25 01:18:37 +0300

    ecomobil

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const tab = document.querySelectorAll(".tab");
+const toggleTab = (el) => {
+    const tabButtons = el.querySelectorAll('.btn-tab');
+    const tabContent = el.querySelectorAll('.tab-content');
+    tabButtons[0].classList.add('active');
+    tabContent[0].classList.add('active');
+
+    const removeTab = (el) => {
+  for (const i of el) {
+    i.classList.remove('active');
+  }
+    };
+
+    const openTab = (index) => {
+        removeTab(tabButtons);
+        removeTab(tabContent);
+        tabButtons[index].classList.add('active');
+        tabContent[index].classList.add('active');
+    };
+    tabButtons.forEach((el, i) => (el.onclick = () =>  openTab(i)));
+
+};
+[...tab].forEach((el) => toggleTab(el))
